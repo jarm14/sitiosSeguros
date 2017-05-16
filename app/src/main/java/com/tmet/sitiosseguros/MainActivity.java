8e8c169b4ad2cabd732a5e904697ff35f9f1ce89
jarm14 <joelram1@hotmail.com>
2017-05-16 16:10:51 -0500

    Lista de sitios
    Creación de método para recuperar datos de la tabla sitio en un
    ArrayList de la clase sitio

--- app/src/main/java/com/tmet/sitiosseguros/MainActivity.java
+++ app/src/main/java/com/tmet/sitiosseguros/MainActivity.java
@@ -4,12 +4,16 @@ import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import java.sql.Connection;
-
-
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    /*Listas a utilizar*/
+    ArrayList<Sitio> lstSitios = new ArrayList<>();
 
     /*Variable para la conexion a SQL*/
     Connection conexion;
@@ -27,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
 
             createAndShowDialog("No se pudo conectar al servidor. Revise su conexión a Internet.", "Error");
         }
+
+        crearListaSitios("");
     }
 
     /*Metodos para la creación de cuadros de dialogo*/
@@ -44,6 +50,41 @@ public class MainActivity extends AppCompatActivity {
         builder.setMessage(message);
         builder.setTitle(title);
         builder.create().show();
+    }
+
+    /*Metodo para la creacion de lista de datos recuperados de la tabla Sitio*/
+    public void crearListaSitios(String tipoDesastre)
+    {
+        ResultSet itemSitio;
+        String sqlSitio="use puntosSeguros; select * from sitio;";
+
+        try {
+
+            Statement statement = conexion.createStatement();
+            itemSitio = statement.executeQuery(sqlSitio);
+
+            while (itemSitio.next())
+            {
+                // navegar por nuestro ResultSet en cada registro, siempre y cuando exista un prox.
+                Sitio tempSitio = new Sitio();
+
+                tempSitio.setId(Integer.parseInt(itemSitio.getString("ID")));
+                tempSitio.setNombre(itemSitio.getString("NOMBRE"));
+                tempSitio.setDetalle(itemSitio.getString("DETALLE"));
+                tempSitio.setUbicacion(itemSitio.getString("UBICACION"));
+                tempSitio.setTipoDesastre(itemSitio.getString("TIPODESASTRE"));
+
+                lstSitios.add(tempSitio);
+            }
+
+        } catch (SQLException se)
+        {
+            createAndShowDialog(se,"Error: ");
+        }catch (Exception ex)
+        {
+            createAndShowDialog(ex,"Error");
+        }
+
     }
 }
 
